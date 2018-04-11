30899f847627cb0507b13846e6c79e2485b21db7
mduberstein <mduberstein@gmail.com>
2018-04-11 10:51:40 -0400

    Added versions for the book text. Corrected XA 105 or 101 @Content warning for Android

--- Chapter04/SizedBoxView/SizedBoxView/SizedBoxView/SizedBoxViewPage.cs
+++ Chapter04/SizedBoxView/SizedBoxView/SizedBoxView/SizedBoxViewPage.cs
@@ -7,15 +7,18 @@ namespace SizedBoxView
     {
         public SizedBoxViewPage()
         {
+            //p. 77
             BackgroundColor = Color.Pink;
 
             Content = new BoxView
             {
+                //p. 77
+                //Color = Color.Accent,
                 Color = Color.Navy,
                 HorizontalOptions = LayoutOptions.Center,
                 VerticalOptions = LayoutOptions.Center,
-                WidthRequest = 200,
-                HeightRequest = 100
+                //WidthRequest = 200,
+                //HeightRequest = 100
             };
         }
     }
