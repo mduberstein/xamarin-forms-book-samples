82711b041fc3ec3f3b2b8c00b9b033d83bcd727b
mduberstein <mduberstein@gmail.com>
2018-05-26 14:50:03 -0400

    ChessboardDynamic: added SizeChanged logging

--- Chapter14/ChessboardDynamic/ChessboardDynamic/ChessboardDynamic/ChessboardDynamicPage.cs
+++ Chapter14/ChessboardDynamic/ChessboardDynamic/ChessboardDynamic/ChessboardDynamicPage.cs
@@ -1,11 +1,15 @@
 using System;
 using Xamarin.Forms;
+using System.Diagnostics;
 
 namespace ChessboardDynamic
 {
     public class ChessboardDynamicPage : ContentPage
     {
         AbsoluteLayout absoluteLayout;
+		int countAbsLayoutSizeChanged = 0;
+		int countContentViewSizeChanged = 0;
+		int countPageSizeChanged = 0;
 
         public ChessboardDynamicPage()
         {
@@ -13,8 +17,9 @@ namespace ChessboardDynamic
             {
                 BackgroundColor = Color.FromRgb(240, 220, 130),
                 HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.Center
+                VerticalOptions = LayoutOptions.Center                              
             };
+			absoluteLayout.SizeChanged += OnAbsoluteLayoutSizeChanged;
 
             for (int i = 0; i < 32; i++)
             {
@@ -33,11 +38,21 @@ namespace ChessboardDynamic
 
             Padding = new Thickness(5, Device.RuntimePlatform == Device.iOS ? 25 : 5, 5, 5);
             Content = contentView;
+			SizeChanged += OnPageSizeChanged;
         }
+
+		void OnAbsoluteLayoutSizeChanged(object sender, EventArgs eventArgs)
+		{
+			var absLayout = (AbsoluteLayout)sender;
+			System.Diagnostics.Debug.WriteLine($"In {nameof(OnAbsoluteLayoutSizeChanged)}: count={countAbsLayoutSizeChanged++}, X={absLayout.X}, Y={absLayout.Y}," +
+			                                   $" Width={absLayout.Width}, Height={absLayout.Height}");
+		}
 
         void OnContentViewSizeChanged(object sender, EventArgs args)
         {
             ContentView contentView = (ContentView)sender;
+			System.Diagnostics.Debug.WriteLine($"In {nameof(OnContentViewSizeChanged)}: count={countContentViewSizeChanged++}, X={contentView.X}, Y={contentView.Y}," +
+								   $" Width={contentView.Width}, Height={contentView.Height}");
             double squareSize = Math.Min(contentView.Width, contentView.Height) / 8;
             int index = 0;
 
@@ -59,5 +74,13 @@ namespace ChessboardDynamic
                 }
             }
         }
+
+		void OnPageSizeChanged(object sender, EventArgs eventArgs) {
+			var page = (ChessboardDynamicPage)sender;
+			Debug.WriteLine($"In {nameof(OnPageSizeChanged)}: count={countPageSizeChanged++}, X={page.X}, Y={page.Y}," +
+											   $" Width={page.Width}, Height={page.Height}");
+			//var callStack = new System.Diagnostics.Debug.StackTrace();
+			//System.Diagnostics.Debug.WriteLine($"StackTrace: {Environment.StackTrace}");
+		}
     }
 }
